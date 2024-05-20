3db68d596cfe8f1087fb32d9f07e10aeb9d331e3
ricardosantanadev <ricardosantanadev@gmail.com>
2024-05-20 18:03:54 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }
+  {
+    path: '', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  { 
+    path: '**', redirectTo: '', pathMatch: 'full' 
+  }
 ];
 
 @NgModule({
--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WrapperComponent } from '../components/wrapper/wrapper.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: WrapperComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
